7c04bb8c4c2e524308e69b5ced29dadbb2ff3251
Vishal Khatal <vishal.khatal2206@gmail.com>
2020-09-20 19:51:00 +0530

    Adding state

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person';
 
-function App() {
+class App extends Component
+ {
+  state = {
+    person :
+    [
+        { name:"A" },
+        { name:"B" }
+    ]    
+  }
+  render()
+  {
   return (
     <div className="App">
       <h1>HI , I am a react app </h1>
-      <Person name="vishal"/>
-      <Person name="Pooja"/>
+      <Person name={this.state.person[0].name}/>
+      <Person name={this.state.person[1].name}/>
 
     </div>
   );
+  }
 }
 
 export default App;
