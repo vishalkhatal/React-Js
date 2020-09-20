53a7158e8c4e32e20ca42390ceb7a9dc55cd4817
Vishal Khatal <vishal.khatal2206@gmail.com>
2020-09-20 20:11:30 +0530

    Added Button event

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -12,11 +12,16 @@ class App extends Component
         { name:"B" }
     ]    
   }
+  greeting=()=>
+  {
+    alert("Success!!");
+  }
   render()
   {
   return (
     <div className="App">
       <h1>HI , I am a react app </h1>
+      <button onClick={this.greeting}>Click on me !!!</button>
       <Person name={this.state.person[0].name}/>
       <Person name={this.state.person[1].name}/>
 
